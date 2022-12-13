72e212ed09668a68c4da72c0cc72402d5be476f3
Najs-Rekin <120388136+Najs-Rekin@users.noreply.github.com>
2022-12-13 19:59:07 +0100

    Add files via upload

---
+++ RPG_project ostateczny ~~Daria/RPG_project/CharacterClass.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_project
+{
+    internal class CharacterClass
+    {
+
+        public static int[] characterClassSelection()
+        {
+            // hp atack moc gold wendka
+            while (true)
+            {
+                
+                Console.WriteLine($"Jesteś postacią w bardzo dziwnym świecie. Zarabiasz pieniądze chodząc do lasu, aby zabić biesy.Twoja postać posiada moc, którą może zużyć na zaklęcia, aby sobie pomóc .");
+                Console.WriteLine($"wybierz klasę postaci!");
+                Console.WriteLine("1 - czarodziejopodobny \t 2 - Wiedźmin \t 3 - Assassin");
+                string inp = Console.ReadLine();
+                if (inp == "1")
+                {
+                    int[] m = { 200, 10, 300, 30, 0 };
+                    return m;
+                }
+                else if (inp == "2")
+                {
+                    int[] w = { 500, 30, 0, 30, 0 };
+                    return w;
+                }
+                else if (inp == "3")
+                {
+                    int[] w = { 350, 15, 150, 30, 0 };
+                    return w;
+                }
+                else
+                {
+                    Console.WriteLine("nie ma takiej postaci");
+                }
+            }
+        }
+        //---------------------------------------------------------
+        public static bool isAlive(int[] character)
+        {
+            if (character[0] <= 0)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ RPG_project ostateczny ~~Daria/RPG_project/mistrz.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_project
+{
+    public class mistrz
+    {
+
+        public static int[] mistrzu(int[] character)
+        {
+            Console.WriteLine("Witaj w pieciarze pradawnego mistrza, wybierz szkolenie !");
+            Console.WriteLine("Lecz pamiętaj, aby zapłacić (starcy nie pracują za darmo)!");
+            Console.WriteLine("1 - trening wytrzymałościowy 20 coin \t 2 - trening podstawowy 15 coin  \t 3 - trening mistrzowski 100 coin \t ");
+
+            int inp = int.Parse(Console.ReadLine());
+            switch (inp)
+            {
+                case 1:
+                    return wytrz(character);
+                case 2:
+                    return podst(character);
+                case 3:
+                    return mis(character);
+
+
+
+            }
+            Console.WriteLine("Opuszczasz pieczare pradawnego mistrza, lecz znajdziesz ją za każdym razem, gdy udasz się w podróż");
+            return character;
+        }
+
+        public static int[] wytrz(int[] character)
+        {
+            if (character[3] >= 20)
+            {
+                Console.WriteLine("Twój trening się zakończył, zyskujesz + 60 mocy, wydajesz 20 coinów");
+                character[3] -= 20;
+                character[2] += 60;
+            }
+            else
+            {
+                Console.WriteLine("Nie masz wystarczjaco coinów");
+            }
+            return character;
+        }
+
+        public static int[] podst(int[] character)
+        {
+            if (character[3] >= 15)
+            {
+                Console.WriteLine("Twój trening się zakończył, zyskujesz + 40 mocy, wydajesz 15 coinów");
+                character[3] -= 15;
+                character[2] += 40;
+            }
+            else
+            {
+                Console.WriteLine("Nie masz wystarczjaco coinów");
+            }
+            return character;
+        }
+
+        public static int[] mis(int[] character)
+        {
+            if (character[3] >= 100)
+            {
+                Console.WriteLine("Twój trening się zakończył, zyskujesz + 140 mocy, wydajesz 100 coinów");
+                character[3] -= 100;
+                character[2] += 140;
+            }
+            else
+            {
+                Console.WriteLine("Nie masz wystarczjaco coinów");
+            }
+            return character;
+        }
+
+
+
+
+
+
+    }
+}
